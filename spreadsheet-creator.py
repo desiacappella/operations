b211bd63fd2ffe163aaf5e6e1596cf8873573891
Sarang Joshi <sarangj@msn.com>
2020-01-27 15:33:32 -0800

    Add overview sheet

--- spreadsheet-creator.py
+++ spreadsheet-creator.py
@@ -4,6 +4,7 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from typing import List
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
@@ -32,6 +33,99 @@ def get_creds():
     return creds
 
 
+def row(text: List):
+    return {
+        "values": [
+            {
+                "userEnteredValue": {
+                    (x["type"] if "type" in x else "stringValue"): x["value"]
+                },
+                "userEnteredFormat": ({
+                    "textFormat": {
+                        "bold": True
+                    }
+                } if x["bold"] else None)
+            }
+            for
+            x in text
+        ]
+    }
+
+
+VAL = "userEnteredValue"
+FORM = "userEnteredFormat"
+HA = "horizontalAlignment"
+TF = "textFormat"
+
+
+def overview_sheet(teams: List[str]):
+    """
+    @returns SHEET 1: Just a basic list of teams
+    """
+
+    team_rows = [
+        {
+            "values": [
+                {
+                    VAL: {"numberValue": idx+1},
+                    FORM: {HA: "CENTER"}
+                },
+                {
+                    VAL: {"stringValue": team},
+                    FORM: {HA: "CENTER"}
+                }
+            ]
+        }
+        for
+        (idx, team) in enumerate(teams)
+    ]
+
+    return {
+        "properties": {
+            "title": "Overview"
+        },
+        "data": [
+            {
+                "startColumn": 0,
+                "startRow": 0,
+                "rowData": [
+                    {
+                        "values": [
+                            {
+                                VAL: {"stringValue": "Line Up Order", },
+                                FORM: {HA: "CENTER",
+                                       TF: {"bold": True}}
+                            },
+                            {
+                                VAL: {"stringValue": "Team Name", },
+                                FORM: {HA: "CENTER",
+                                       TF: {"bold": True}}
+                            },
+                            {
+                                VAL: {"stringValue": "Placings", },
+                                FORM: {HA: "CENTER",
+                                       TF: {"bold": True}}
+                            },
+                        ]
+                    }
+                ] + team_rows
+            }
+        ]
+    }
+
+
+def create_sheet(sheet_service, judges: List[str], teams: List[str]):
+    properties = {"title": "Test Spreadsheet"}
+    sheets = [
+        overview_sheet(teams)
+    ]
+
+    result = sheet_service.create(
+        body={"properties": properties, "sheets": sheets}, fields="spreadsheetId").execute()
+
+    return result.get('spreadsheetId') if result else None
+
+
 def main():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
@@ -41,16 +135,20 @@ def main():
         sys.stderr.write("Unable to get credentials")
         return 1
 
-    service = build('sheets', 'v4', credentials=creds)
+    judges = [f"Judge {x+1}" for x in range(4)]
+    teams = [f"Team {x+1}" for x in range(5)]
 
-    # Call the Sheets API
-    sheet = service.spreadsheets()
+    # Set up Google Sheets service
+    # pylint: disable=no-member
+    sheet = build('sheets', 'v4', credentials=creds).spreadsheets()
 
-    result = sheet.create(body={"properties": {
-                          "title": "Test Spreadsheet"}}, fields="spreadsheetId").execute()
+    result = create_sheet(
+        sheet, judges, teams
+    )
 
     if result:
-        print(result.get("spreadsheetId"))
+        print(
+            f"https://docs.google.com/spreadsheets/d/{result}/edit")
     else:
         print("result null")
 
