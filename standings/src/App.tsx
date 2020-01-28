dd137f3f4771073651d12f181bc0b455d27ada29
Sarang Joshi <sarangj@msn.com>
2020-01-28 05:28:32 -0800

    Add standings view

---
+++ standings/src/App.tsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import { map } from "lodash";
+import { Grid, Typography, Slider } from "@material-ui/core";
+
+import details from "./details.json";
+
+const valuetext = (value: number) => details.order[value];
+
+const App: React.FC = () => {
+  const [num, setNum] = useState(details.order.length);
+  const [allViews, setAllViews] = useState([[]] as any[]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:5000");
+      const t: Array<Record<number, Array<string>>> = await res.json();
+      console.log(t);
+      const mapped = map(t, thresholds =>
+        map(thresholds, (teams, threshold) => ({
+          threshold,
+          teams
+        }))
+      );
+      console.log(mapped);
+      setAllViews(mapped);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleChange = ({}, newValue: number | number[]) => {
+    setNum(newValue as number);
+  };
+
+  // currentView is an array of { threshold: T, teams: [] } objects
+  const currentView = num < allViews.length ? allViews[num] : [];
+
+  return (
+    <div style={{ textAlign: "center", padding: "0 40%", margin: "5px" }}>
+      <Typography variant="h2">ASA Standings 2018-19</Typography>
+      <Slider
+        value={num}
+        onChange={handleChange}
+        getAriaValueText={valuetext}
+        valueLabelDisplay="auto"
+        step={1}
+        marks={details.order.map((label, i) => ({ value: i + 1, label }))}
+        min={0}
+        max={details.order.length}
+      />
+      <Grid container>
+        <Grid xs={1} item>
+          <Typography>T</Typography>
+        </Grid>
+        <Grid xs item>
+          <Typography>Teams</Typography>
+        </Grid>
+      </Grid>
+      {currentView.map((t: { threshold: number; teams: string[] }) => (
+        <Grid container>
+          <Grid xs={1} item>
+            <Typography>{t.threshold}</Typography>
+          </Grid>
+          {t.teams.map((g: any) => (
+            <Grid xs item style={{ border: "1px solid black" }}>
+              <Typography>{g}</Typography>
+            </Grid>
+          ))}
+        </Grid>
+      ))}
+    </div>
+  );
+};
+
+export default App;
